765ec197a35f658a875c750ef6ec8df2e5e8b060
KigoryGH <alialmowali213@protonmail.com>
2026-09-16 14:19:48 +0300

    idklol

--- main.go
+++ main.go
@@ -1,9 +1,25 @@
 package main
 
-// imports
 import (
-	//"fmt"
-	//tcell "github.com/gdamore/tcell/v2"
+	tcell "github.com/gdamore/tcell/v2"
 )
+
 func main() {
+	screen, err := tcell.NewScreen()
+	if err != nil {
+		panic(err)
+	}
+
+	err = screen.Init()
+	if err != nil {
+		panic(err)
+	}
+
+			screen.PollEvent()
+		ev := screen.PollEvent()
+
+		switch ev.(type) {
+case *tcell.EventKey:
+}
+		screen.Fini()
 }
