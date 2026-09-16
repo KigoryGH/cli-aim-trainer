5b18e1dab127d23dca1d5013a09e072fa04c4776
KigoryGH <alialmowali213@protonmail.com>
2026-09-16 21:43:45 +0300

    idk lol

--- main.go
+++ main.go
@@ -15,15 +15,18 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+	width, height := screen.Size()
+x := rand.Intn(width)
+y := rand.Intn(height)
+	screen.SetContent(x, y, 'X', nil, tcell.StyleDefault)
+	
+	screen.Show()
 
 		ev := screen.PollEvent()
 
 		switch ev.(type) {
 case *tcell.EventKey:
+	screen.Fini()
+	return
 }
-width, height := screen.Size()
-x := rand.Intn(width)
-y := rand.Intn(height)
-screen.SetContent(x int(width), y int(height)
-		screen.Fini()
 }
